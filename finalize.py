1f98d31fdb0c22b055993c677f1e924feaacb7dd
Brad Killeen <bkilleen@rh.com>
2024-05-17 14:52:13 -0600

    awljdghuawkjldygawduy

--- finalize.py
+++ finalize.py
@@ -12,20 +12,42 @@ approved_params = {
 	]
 }
 
-gx = galaxy_api_class.gx_api(production=True)
+gx = galaxy_api_class.gx_api(production=False)
 res = gx.find_records(approved_params)
 #print(json.dumps(res, indent=4))
 wip_paths = [i['fieldData']['WIPS_PATH'] for i in res['response']['data']]
-with open('wips.txt','w') as my_file:
+with open('wips.txt','w') as wips_file:
 	for i,path in enumerate(wip_paths):
 		path = path.replace(":","/").strip()
 		path = f'/Volumes/{path}'
-		my_file.write(f'{path}\n')
-		if i > 1:
+		
+		if i == len(wip_paths) - 1:
+			wips_file.write(f'{path}')
+		else:
+			wips_file.write(f'{path}\n')
+
+#		if i > 1:
+#			break
+
+wips_file.close()
+
+# subprocess.run(args=['/Applications/Adobe Photoshop 2024/Adobe Photoshop 2024.app/Contents/MacOS/Adobe Photoshop 2024','-r',os.path.expanduser("~/finalizations/finalize_assets.jsx")])
+
+with open('finals.txt', 'r') as finals_file:
+	for line in finals_file:
+		filename = line.strip()
+		res = gx.find_records(params = {'query':[{'cRetoucher_ ImageName': filename,'omit': "false"}]})
+		recordId = [i['recordId'] for i in res['response']['data']]
+		wip_path = [i['fieldData']['WIPS_PATH'] for i in res['response']['data']]
+
+		if len(recordId) == 1 and len(wip_path) == 1:
+			final_path = wip_path[0].replace("WIPS","FINAL").replace(".psb",".tif")
+			gx.update_record(recordId[0], data={"RetouchStatus":"AutoCompleted", "FINAL_PATH":final_path})
+		else:
+			print('problems') # Add logic to handle redundant GX filenames
 			break
+		
+		#data = {"RetouchStatus":"Ready for Retouching", "OutlinePath" : i[1].replace(".tif",extension).replace("Processed","Outlines")}
 
-my_file.close()
-
-subprocess.run(args=['/Applications/Adobe Photoshop 2024/Adobe Photoshop 2024.app/Contents/MacOS/Adobe Photoshop 2024','-r',os.path.expanduser("~/finalizations/finalize_assets.jsx")])
-
-with open(finals.txt)
+		#print(json.dumps(res, indent=4))
+		#print(recordId)
